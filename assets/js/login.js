85934d264fa23ccf492a99de56c2341e08f1f873
dong19972457 <dong19972457@163.com>
2021-11-01 21:36:44 +0800

    first commit

---
+++ assets/js/login.js
@@ -0,0 +1,66 @@
+$(function() {
+    // 点击“去注册账号”的链接
+    $("#link_reg").on("click", function() {
+        $(".login-box").hide();
+        $(".reg-box").show();
+    })
+
+    // 点击"去登录"链接
+    $("#link_login").on("click", function() {
+        $(".login-box").show();
+        $(".reg-box").hide();
+    })
+
+
+    // 从layui中获取form对象
+    var layer = layui.layer;
+    var form = layui.form;
+    // 通过form.verify()自定义校验规则
+    form.verify({
+            // 自定义一个叫做pwd的校验规则
+            pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+            // 校验两次密码是否一致
+            repwd: function(value) {
+                // 通过形参拿到的是确认密码框中的值
+                var pwd = $('.reg-box [name=password]').val();
+                if (pwd !== value) {
+                    return '密码不一致！';
+                }
+            }
+        })
+        // 监听注册表单的提交事件
+    $("#form_reg").on("submit", function(e) {
+            // 阻止默认的提交行为
+            e.preventDefault();
+            // 发起Ajax的POST请求
+            var data = { username: $("#form_reg [name=username]").val(), password: $("#form_reg [name=password]").val() };
+            $.post("/api/reguser", data,
+                function(res) {
+                    if (res.status !== 0) {
+                        return layer.msg(res.message);
+                    }
+                    layer.msg("注册成功,请登录");
+                    // 模拟去登录按钮的点击行为
+                    $("#link_login").click();
+                })
+        })
+        // 监听登录表单的提交事件
+    $("#form_login").submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function(res) {
+                if (res.status !== 0) {
+                    return layer.msg("登录失败");
+                }
+                layer.msg("登录成功");
+                // 登录成功后，将登录成功得到的token字符串保存到localStorage中
+                localStorage.setItem("token", res.token);
+                // 跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+})
